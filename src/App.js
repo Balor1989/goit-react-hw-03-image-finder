0d4dc19ae8763de623e823392f79b4d007430a4c
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-30 11:25:17 +0200

    adds fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Component } from "react/cjs/react.production.min";
+import axios from "axios";
 
 
-function App() {
-  return (
-    <>
+class App extends Component {
+
+  async componentDidMount() {
+    try {
+     const response = await axios.get('https://pixabay.com/api/?q=cat&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12')
+      const data = response.data
+      console.log(data)
+     return data 
+    }
+    catch (error) {
+      console.log(error)
+      return Promise.reject(error)
+    }
+  }
+  
+
+  render() {
+    return (
       <h1>Hello!</h1>
-    </>
-  );
+    )
+  }
 }
 
 export default App;
