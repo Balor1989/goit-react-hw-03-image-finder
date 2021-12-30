265928527d505a5cf9257b0bdb91e84313fa2569
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-30 14:08:36 +0200

    adds handles

--- src/Searchbar/Searchbar.js
+++ src/Searchbar/Searchbar.js
@@ -5,14 +5,29 @@ import { FcSearch} from 'react-icons/fc';
 
 class Searchbar extends Component {
 
-
+    state = {
+        searchValue: ''
+    }
+    
+    handleChange = (e) => {
+        this.setState({searchValue: e.currentTarget.value })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("submit")
+        this.setState({ searchValue: "" });
+    }
 
     render() {
         return (
        
-            <form>
-                <input type="text" />
-                <button type="button">
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                    placeholder="Search images"
+                    value={this.setState.searchValue}
+                onChange={this.handleChange}
+                />
+                <button type="submit">
                     <FcSearch style={{ width: 25, height: 25 }} />
                 </button>
             </form>
