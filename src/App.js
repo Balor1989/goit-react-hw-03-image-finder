d815178895daaa94e77e9a3e7868bdab271eb053
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-30 16:39:58 +0200

    adds form input request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react/cjs/react.production.min";
-import axios from "axios";
 import Searchbar from "./Searchbar/Searchbar";
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,18 +15,6 @@ class App extends Component {
   handleFormSubmit = value => {
     this.setState({ searchValue: value })
   };
-
-  async componentDidMount() {
-    try {
-     const response = await axios.get('https://pixabay.com/api/?q=cat&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12')
-      const pictures = await response.data
-     return this.setState({pictures})
-    }
-    catch (error) {
-      console.log(error)
-      return Promise.reject(error)
-    }
-  }
 
   render() {
     return (
