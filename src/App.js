5d8d066c226b7fd37566d2892226d8550b71f4fe
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-31 10:18:00 +0200

    save changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from "./ImageGallery/ImageGallery";
 import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
+import axios from "axios";
 
 
 class App extends Component {
 
   state = {
     searchValue: '',
+    pictures: null
   };
 
   handleFormSubmit = value => {
     this.setState({ searchValue: value })
   };
+   async componentDidUpdate(prevProps, prevState) {
+     if (prevState.searchValue !== this.state.searchValue) {
+          console.log("111")
+          try {
+              const response = await axios.get(`https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
+              const pictures = await response.data
+              return this.setState({ pictures })
+            }
+          catch (error) {
+              console.log(error);
+              return Promise.reject(error);
+            }
+        };
+    };
 
   render() {
     return (
@@ -23,7 +39,7 @@ class App extends Component {
         <ToastContainer />
 
         <Searchbar formSubmit={this.handleFormSubmit} />
-        <ImageGallery values={this.state.searchValue}>
+        <ImageGallery >
           <h2>Gallery</h2>
           <ImageGalleryItem />
         </ImageGallery>
--- src/ImageGallery/ImageGallery.js
+++ src/ImageGallery/ImageGallery.js
@@ -1,25 +1,9 @@
 import { Component } from "react/cjs/react.production.min";
-import axios from "axios";
+
 
 class ImageGallery extends Component {
 
-    state = {
-        pictures: null
-    }
 
-    async componentDidUpdate(prevProps) {
-        if (prevProps.values !== this.props.values) {
-          try {
-              const response = await axios.get(`https://pixabay.com/api/?q=${this.props.values}&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
-              const pictures = await response.data
-              return this.setState({ pictures })
-            }
-    catch (error) {
-              console.log(error);
-              return Promise.reject(error);
-            }
-        };
-    };
 
     render() {
         return (
