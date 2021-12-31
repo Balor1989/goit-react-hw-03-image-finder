617187306243da48629b59b1732d270946f029b0
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-31 13:08:28 +0200

    adds "Load more" button

--- src/App.js
+++ src/App.js
@@ -5,39 +5,44 @@ import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from "./ImageGallery/ImageGallery";
 import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
 import axios from "axios";
+import Button from "./Button/Button";
 
 
 class App extends Component {
 
   state = {
     searchValue: '',
-    pictures: null,
     arrayOfImages:[]
   };
+
+  handleClickLoadMore = (e) => {
+   console.log("Load more")
+ }
 
   handleFormSubmit = value => {
     this.setState({ searchValue: value })
   };
-   async componentDidUpdate(prevProps, prevState) {
-     if (prevState.searchValue !== this.state.searchValue) {
-          try {
-              const response = await axios.get(`https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
-            const pictures = await response.data
-            if (pictures.totalHits === 0) {
-              toast.info('No pictures found for your request')
-              return
-                 }
-              this.setState((prevState) => ({
-              arrayOfImages: [...prevState.arrayOfImages, ...pictures.hits],
-            }));
-            }
-          catch (error) {
-              console.log(error);
-              return Promise.reject(error);
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchValue !== this.state.searchValue) {
+        try {
+            const response = await axios.get(`https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
+          const pictures = await response.data
+          if (pictures.totalHits === 0) {
+            toast.info('No pictures found for your request')
+            return
+                }
+            this.setState((prevState) => ({
+            arrayOfImages: [...prevState.arrayOfImages, ...pictures.hits],
+          }));
           }
-       
-     };
+        catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+      
     };
+  };
 
   render() {
     return (
@@ -50,6 +55,7 @@ class App extends Component {
           <h2>Gallery</h2>
           <ImageGalleryItem images={this.state.arrayOfImages}/>
         </ImageGallery>
+        <Button onHandleClickLoadMore={this.handleClickLoadMore}/>
         </>
     )
   }
---
+++ src/Button/Button.js
@@ -0,0 +1,8 @@
+
+const Button = ({onHandleClickLoadMore}) => (
+    <div className="button-box">
+        <button type="button" className="button" onClick={onHandleClickLoadMore}>Load more</button>
+    </div>
+);
+
+export default Button
