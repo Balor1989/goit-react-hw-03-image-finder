0e01a0ddda65379bfcd8b6f5b7b4bd201830c0cf
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-12-31 11:28:23 +0200

    adds notification if no pictures were found

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react/cjs/react.production.min";
 import Searchbar from "./Searchbar/Searchbar";
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from "./ImageGallery/ImageGallery";
 import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
@@ -23,11 +23,13 @@ class App extends Component {
           try {
               const response = await axios.get(`https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=23825879-78d35eabdb1bf9c22a9a5e768&image_type=photo&orientation=horizontal&per_page=12`)
             const pictures = await response.data
-
+            if (pictures.totalHits === 0) {
+              toast.info('No pictures found for your request')
+              return
+                 }
               this.setState((prevState) => ({
               arrayOfImages: [...prevState.arrayOfImages, ...pictures.hits],
             }));
-              return this.setState({ pictures })
             }
           catch (error) {
               console.log(error);
